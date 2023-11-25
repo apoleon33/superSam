93e7fb67805294d3aa1b4f18b5f430eec6048f3c
apoleon33 <simondumesny@gmail.com>
2023-11-25 13:50:49 +0100

    config file

---
+++ config.py
@@ -0,0 +1,9 @@
+HEIGHT: int = int(1080 / 1.25)
+WIDTH: int = int(HEIGHT * (16 / 9))
+
+FPS: int = 80
+GRAVITY: int = int(HEIGHT * (5 / (1080 / 2)))
+
+MAIN_CHARACTER_HEIGHT: int = int(HEIGHT * (100 / (1080 / 2)))
+MAIN_CHARACTER_WIDTH: int = int(MAIN_CHARACTER_HEIGHT * 0.8)
+MAIN_CHARACTER_SPEED: int = int(WIDTH * (5 / (1920 / 2)))
