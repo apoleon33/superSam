2098ba434ff301b8439ca0aa249527a0c38bb79b
Louis-Sieurac <louissieurac33@gmail.com>
2023-11-28 12:25:06 +0100

    fix

--- Map/elevator.py
+++ Map/elevator.py
@@ -4,3 +4,4 @@ from Map.tunnel import Tunnel
 class Elevator(Tunnel):
     def __init__(self):
         super().__init__("elevator")
+        self.Image = Image("assets/blocks/tunnel/elevator/elevator.png")
