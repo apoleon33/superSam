e9354e40555f5985259c83830b73c486a87c1bff
Louis-Sieurac <louissieurac33@gmail.com>
2023-12-07 11:15:38 +0100

    new size

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ WIDTH: int = int(HEIGHT * (16 / 9))
 FPS: int = 80
 GRAVITY: int = int(HEIGHT * (5 / (1080 / 2)))
 
-MAIN_CHARACTER_HEIGHT: int = int(HEIGHT * (100 / (1080 / 2)))
-MAIN_CHARACTER_WIDTH: int = int(MAIN_CHARACTER_HEIGHT * 0.8)
+MAIN_CHARACTER_HEIGHT: int = int(64)
+MAIN_CHARACTER_WIDTH: int = int(33)
 MAIN_CHARACTER_SPEED: int = int(WIDTH * (5 / (1920 / 2)))
 
 HITBOX = False
